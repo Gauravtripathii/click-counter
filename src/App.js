57e93298327ee0bf27b11ed2b19bc1c2bda3617f
Gauravtripathii <gauravtripathii7979@gmail.com>
2022-11-02 19:29:24 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import { useState } from "react";
+
 function App() {
+  const [click, setClick] = useState(0);
   return (
     <div className="app">
-      <h1></h1>
+      <h1>count : {click}</h1>
+      <span onClick={() => setClick(click + 1)}>+</span>
     </div>
   );
 }
